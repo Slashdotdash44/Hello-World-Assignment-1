561b986d2de6f1e6bcc34feaf8a4c7317bc54d8e
Slashdotdash44 <35644648+Slashdotdash44@users.noreply.github.com>
2018-01-20 20:00:36 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+/*Author: Alexander Javor
+ *Date: January 20 2018
+ *
+ *This is a simple program that is meant to express a basic understanding
+ *of how to use GitHub for team-based work.
+ */
+
+#include "stdio.h"
+
+int main(void) {
+  printf("Alexander Javor Assignment one\n");
+  return 0;
+}
